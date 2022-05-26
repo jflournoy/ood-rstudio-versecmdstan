3a2ca4b1f54ce209004c9395bf63d5b21c583bd9
John Flournoy <john_flournoy@fas.harvard.edu>
2022-05-26 17:17:42 -0400

    generalized

--- sbatch_build_verse-cmdstan.bash
+++ sbatch_build_verse-cmdstan.bash
@@ -4,9 +4,8 @@
 #SBATCH --mem=16G
 #SBATCH -t 04:00:00
 #SBATCH -p fasse
-#SBATCH -J build
-#SBATCH --account=mclaughlin_lab
+#SBATCH -J singbuild
 # Outputs ----------------------------------
-#SBATCH -o /n/home_fasse/jflournoy/data/containers/log/%A_%a-%x.out
+#SBATCH -o %A_%a-%x.out
 
 srun -c 1 singularity build --sandbox verse-cmdstan-$1.simg docker://jflournoy/verse-cmdstan:$1
